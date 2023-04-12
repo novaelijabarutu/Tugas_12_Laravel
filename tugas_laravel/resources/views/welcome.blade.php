38513a6f7abc3cc5778559a2d6a64ba2c363a916
Nova Elija Barutu <novaelija079@gmail.com>
2023-04-12 22:47:53 +0700

    membuat web statis <novaelijabarutu>

---
+++ tugas_laravel/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" cotent="width=device-width,initial-scale=1.0">
+	<title>materi 12</title>
+</head>
+<body>
+	<h1>Welcome, {{$First_name}} {{$Last_name}}</h1>
+	<p> Terima Kasih Telah bergabung bersama Garuda Cyber Institute </p>
+</body>
+</html>
